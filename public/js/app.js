d62ea7c6f05effdae20ee1fbe860d212aa24024f
Adrenine <adrenine13@gmail.com>
2020-10-23 01:28:56 +0530

    latest commit before deployment to heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,14 +8,12 @@ const messageTwo = document.querySelector('#message2');
 
 weatherForm.addEventListener('submit', (event) => {
   const location = search.value;
-  console.log(location);
   event.preventDefault();
   messageOne.textContent = "Loading";
   messageTwo.textContent =null;
   fetch("http://localhost:3000/weather?address="+location).then((response) => {
   response.json().then((data) => {
     if (data.error) {
-      console.log(data)
       messageOne.textContent = data.error;
     } else {
       messageOne.textContent = data.forecast;
